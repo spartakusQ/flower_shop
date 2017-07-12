5835fd5cac6884de81f5194d070ef96c15915ee4
spartakusQ <spartakus1688@gmail.com>
2017-07-12 04:51:36 +0300

    New root to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  root 'flower_pages#home'
+
+  get 'flower_pages/help'
+
+  get 'flower_pages/about'
+
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
